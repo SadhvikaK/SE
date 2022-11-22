5cd8e6de5213b4ccd55bf477731a244dfbcabdfa
SadhvikaK <sadhvika02@gmail.com>
2022-11-22 17:06:52 +0530

    Demo for Team

---
+++ Second/src/test/java/com/app/maven/Second/AppTest.java
@@ -0,0 +1,20 @@
+package com.app.maven.Second;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
